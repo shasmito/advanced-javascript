3051af31262c7da7554bf605d9b7203b60da6eb3
Pallab Sarkar <59387756+shasmito@users.noreply.github.com>
2020-08-11 02:46:22 +0600

    True False

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// Falsy
+// 0
+// ""
+// undefine
+// null
+// NaN
+// false
+
+
+// Truthy
+// "0", " " ,[], {}
+
+// const name = " ";
+let name = 12;
+if (name || name == 0){
+console.log(" condition is true");
+}
+else{
+    console.log("condition is false");
+}
