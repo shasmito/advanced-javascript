88d82fbf1d9809fdc616ba977f92e6a213c7a00b
Pallab Sarkar <59387756+shasmito@users.noreply.github.com>
2020-08-11 03:00:10 +0600

    null-vs-undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,12 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+// const result = add(13, 12);
+// console.log(result);
+
+// const premik = {name: "smart dude", phone: 0154165210};
+// console.log(premik.gf);
+
+let fun = undefine;
+console.log(fun);
