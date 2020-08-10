4666f038640b3f15e3309ca99dc58a6d97b43469
Pallab Sarkar <59387756+shasmito@users.noreply.github.com>
2020-08-11 03:13:46 +0600

    double-vs-triple-equal

--- null-vs-undefine.js
+++ null-vs-undefine.js
@@ -1,12 +1,14 @@
-// let pakhi;
-// function add(num1, num2){
-//     console.log(num1 + num2);
-// }
-// const result = add(13, 12);
-// console.log(result);
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+}
 
-// const premik = {name: "smart dude", phone: 0154165210};
-// console.log(premik.gf);
+const result = add(13, 12);
+console.log(result);
 
-let fun = undefine;
+const premik = {name: "smart dude", phone: 0154165210};
+console.log(premik.gf);
+
+let fun = undefined;
 console.log(fun);
+
